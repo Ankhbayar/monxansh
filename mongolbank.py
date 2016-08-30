a087fdf9513e71cad8e4a4482ed51037e85ae6d1
Amar Battulga <battulga_amar@unimedia.co.jp>
2016-08-30 15:47:09 +0900

    refactoring

--- mongolbank.py
+++ mongolbank.py
@@ -3,27 +3,19 @@
     Монгол банкны ханш татагч
     Author : @Ankhbayar
 """
-
 import re
+from google.appengine.api import urlfetch
+
 
 CURRENCY_RATE_URL = "http://www.mongolbank.mn/dblistofficialdailyrate.aspx"
-# Thanks. @ubs121
-REGX_PATTERN = u"<tr>\\s*<td.*>.*</td>\\s*<td.*>(?P<name>.*)</td>\\s*<td.*><span id=\"ContentPlaceHolder1_lbl(?P<code>\\w{3})\">(?P<rate>.*)</span></td></tr>"
-
-
-def open_url_data(url):
-    from google.appengine.api import urlfetch
-    result = urlfetch.fetch(CURRENCY_RATE_URL, deadline=30, headers={
-        "Cache-Control": "no-cache,max-age=0",
-        "Pragma": "no-cache"
-    })
-    return result.content
-
 
 def get_data():
-    html_text = open_url_data(CURRENCY_RATE_URL)
-    if html_text is not None:
-        pattern = re.compile(REGX_PATTERN)
-        return [r.groupdict() for r in pattern.finditer(html_text)]
-    else:
-        return "error mb connect error"
+    r = urlfetch.fetch("http://www.mongolbank.mn/dblistofficialdailyrate.aspx")
+
+    assert r.status_code == 200
+
+    regex = re.compile(u"<tr>\\s*<td.*>.*</td>\\s*<td.*>(?P<name>.*)</td>\\s*<td.*>"
+                       u"<span id=\"ContentPlaceHolder1_lbl(?P<code>\\w{3})\">"
+                       u"(?P<rate>.*)</span></td></tr>")
+
+    return [r.groupdict() for r in regex.finditer(r.content)]
